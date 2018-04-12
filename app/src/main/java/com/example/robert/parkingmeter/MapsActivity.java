f09478660ac18b0689f093547b93952f5b04bb54
Robert Kahmann <robert.kahmann@inovexcorp.com>
2018-04-12 11:01:26 -0400

    Trying to connect to URL to view JSON.

--- app/src/main/java/com/example/robert/parkingmeter/MapsActivity.java
+++ app/src/main/java/com/example/robert/parkingmeter/MapsActivity.java
@@ -1,7 +1,19 @@
 package com.example.robert.parkingmeter;
 
+
+import android.os.AsyncTask;
+import android.support.annotation.RequiresPermission;
+import android.support.v7.app.AppCompatActivity;
+import android.app.ProgressDialog;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.view.View;
+import android.widget.TextView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -9,10 +21,24 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.sql.Ref;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+
+    TextView txtJson;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +48,10 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        txtJson = (TextView) findViewById(R.id.tvJsonItem);
+        new JsonTask().execute("Url address here");
+
     }
 
 
@@ -38,9 +68,14 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        // Add a marker in US and move the camera
+        LatLng US = new LatLng(39, -84);
+        mMap.addMarker(new MarkerOptions().position(US).title("Marker in US"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(US));
+        mMap.addMarker(new MarkerOptions()
+                .position(new LatLng(39, -84.5))
+                .title("Available")
+                .snippet("and snippet")
+                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN)));
     }
 }
